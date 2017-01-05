3c3b96a5a698cd3bf1104ffdc96a601d5a2ab3b5
Prahlad Yeri <prahladyeri@yahoo.com>
2017-01-05 16:43:03 +0530

    Updated .gitignore, made some changes

--- config.py
+++ config.py
@@ -1,8 +1,22 @@
 # Enter duration in minutes
 schedule = [
-	{"name":"idle", "category":"", "duration":30},
-	{"name":"work on pomodoro script", "category":"pomodoro", "duration":30},
-	{"name":"work on job search", "category": "freelance-jobsearch", "duration":30},
+	# {"name":"idle", "category":"", "duration":2},
+	# {"name":"next idle", "category":"", "duration":2},
+	#{"name":"find jobs on upwork", "category": "freelance-jobsearch", "duration":30},
+	#{"name":"tea break", "category": "tea break", "duration":15},
+	#{"name":"complete article on php drive example", "category": "blogging", "duration":30},
+	{"name":"Solve puzzles at freecodecamp.", "category": "programming", "duration":60},
+	{"name":"Solve puzzles at kattis.", "category": "programming", "duration":60},
+	{"name":"Improve website.", "category": "webdev", "duration":45},
+	{"name":"Work on VA.", "category": "webdev", "duration":60},
+	{"name":"revise java", "category": "programming-java", "duration":30},
+	{"name":"revise android", "category": "programming-android", "duration":30},
+	{"name":"Contribute an article to wikipedia.", "category": "writing", "duration":30},
+	# {"name":"revise webdev", "category": "programming-php", "duration":30},
 ]
 
 slot_interval = 30 #minutes
+
+
+# {"name":"implement killall.py in python", "category": "programming-python", "duration":30},
+# {"name":"implement purify.py in python (script to standardize leading tabs)", "category": "programming-python", "duration":30},
--- pomodoro.py
+++ pomodoro.py
@@ -33,7 +33,7 @@ def select_schedule():
 		print("[%d] %s:%s (%s mins)" % (i, item['category'], item['name'], item['duration']))
 		i += 1
 	while True:
-		num = int(input("\n Select schedule:"))
+		num = int(input("\nSelect schedule:"))
 		if num in range(1, len(config.schedule)+1):
 			return config.schedule[num-1]
 		else:
@@ -41,30 +41,53 @@ def select_schedule():
 
 def start_tracking(task):
 	global last_beep
-	print("Working on %s:%s" % (task['category'], task['name']))
-	print("Started tracking at %s" % (datetime.datetime.now().strftime("%H:%M")))
-	print("Beep interval is set to %d" % config.slot_interval)
+	print("Working on %s:%s (%d minutes)." % (task['category'], task['name'], task['duration']))
+	print("Started tracking at %s." % (datetime.datetime.now().strftime("%H:%M")))
+	print("Beep interval is set to %d minutes." % config.slot_interval)
 	session_start_time = datetime.datetime.now()
+	task_end_time = session_start_time + datetime.timedelta(minutes=task['duration'])
 	last_beep = datetime.datetime.now()
+	notified = False
 	while(True):
-		diff = (datetime.datetime.now() - last_beep).total_seconds() / 60.0 #in minutes
+		now = datetime.datetime.now()
+		diff = (now - last_beep).total_seconds() / 60.0 #in minutes
+		minutes_worked = round(diff)
+		reminder_text = "%s:%s" % (task['category'], task['name'])
 		#diff = diff.total_seconds() / (60.0) 
 		if (diff >= config.slot_interval): #30
-			reminder_text = "%s:%s" % (task['category'], task['name'])
-			diff = (datetime.datetime.now() - session_start_time)
+			#notify
+			diff = (now - session_start_time)
 			diff = diff.total_seconds() / (60.0) #in minutes
-			minutes_worked = round(diff)
-			winsound.Beep(700,800)
+			winsound.Beep(900,800)
 			print("Reminder for %s" % reminder_text)
-			print("Minutes worked in this session: %s" % minutes_worked)
-			print("*******")
+			print("Minutes worked in this session: %s." % minutes_worked)
+			print("****")
 			timesheet.append({
 			"task": task,
 			"start_time": last_beep,
 			"end_time": datetime.datetime.now()
 			})
 			pickle.dump(timesheet, open('timesheet.pkl','wb'),-1)
-			last_beep = datetime.datetime.now()
+			last_beep = now
+		elif (now >= task_end_time and notified == False):
+			print("Task complete: %s." % reminder_text)
+			diff = (now - session_start_time)
+			diff = diff.total_seconds() / (60.0) #in minutes
+			winsound.Beep(900,1400)
+			print("Minutes worked in this session: %s." % minutes_worked)
+			print("****")
+			notified = True
+			last_beep = now
+			#TODO: Don't just continue working on this same task. Mark this task as complete, then offer user to move to the next task, or quit.
+			x = input("Do you want to mark this as complete and move to the next task (Y/n)?")
+			if x.lower() == "y":
+				idx = config.schedule.index(task) + 1
+				if idx > (len(config.schedule) - 1):
+					print("This was the last task in the schedule.")
+					break
+				else:
+					print("Switching to next Task.")
+					start_tracking(config.schedule[idx])
 
 def main():
 	global project_details, last_beep
