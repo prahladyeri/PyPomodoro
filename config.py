4a0e3ebdf57f3919f31eaf385846c758aa04a24c
Prahlad Yeri <prahladyeri@yahoo.com>
2018-05-16 01:57:43 +0530

    updates

--- config.py
+++ config.py
@@ -1,38 +1,7 @@
 # Enter duration in minutes
 schedule = [
-	#~ {"name":"Test", "category": "programming", "duration":1},
-	{"name":"Free code camp", "category": "programming", "duration":30},
-	{"name":"Find jobs", "category": "programming", "duration":30},
-	{"name":"Work - FAS", "category": "programming", "duration":30},
-	{"name":"Work - exch-rate", "category": "programming", "duration":30},
-	{"name":"Publish - Create videos - Upwork intro", "category": "programming", "duration":30},
-	#~ {"name":"Work on wmt.", "category": "programming", "duration":100},
-	#~ {"name":"Work on twitter-tango", "category":"programming", "duration":25},
-	#~ {"name":"reading javascript", "category":"study", "duration":25},
-	#~ {"name":"Work on gearcompare.", "category": "programming", "duration":25},
-	#~ # {"name":"idle", "category":"", "duration":2},
-	#~ #{"name":"find jobs on upwork", "category": "freelance", "duration":25},
-	#~ #{"name":"upwork exams", "category": "freelance", "duration":60},
-	#~ #{"name":"read book on web designing", "category": "study", "duration":50},
-	#~ #{"name":"Work on viz app.", "category": "programming", "duration":60},
-	#~ {"name":"work on zentathedog's project.", "category": "programming", "duration":25},
-	#~ {"name":"work on base32_html.", "category": "programming", "duration":25},
-	#~ {"name":"learn inkscape", "category": "programming", "duration":40},
-	#~ {"name":"Solve puzzles at kattis.", "category": "programming", "duration":40},
-	#~ {"name":"Solve puzzles at freecodecamp.", "category": "programming", "duration":25},
-	#~ {"name":"Work on VA.", "category": "programming", "duration":60},
-	#~ {"name":"revise java", "category": "programming", "duration":30},
-	#~ {"name":"revise android", "category": "programming", "duration":30},
-	#~ {"name":"Contribute article to wikipedia.", "category": "writing", "duration":30},
-	#~ {"name":"Contribute answer to stackoverflow.", "category": "writing", "duration":30},
-	#~ {"name":"Improve website.", "category": "webdev", "duration":30},
-	#~ # {"name":"learn laravel", "category": "study", "duration":30},
-	#~ #{"name":"GV26 Accupressure", "category": "", "duration":3},
+	{"name":"idle", "category":"", "duration":15},
+	{"name":"find jobs on upwork", "category": "freelance", "duration":25},
 ]
 
-slot_interval = 300 #minutes
-
-
-#{"name":"work on prototype-101-ci3.", "category": "programming", "duration":25},
-# {"name":"implement killall.py in python", "category": "programming-python", "duration":30},
-# {"name":"implement purify.py in python (script to standardize leading tabs)", "category": "programming-python", "duration":30},
+slot_interval = 25 #minutes
