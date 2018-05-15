730271f9375300d2ecfedbb9c86c0d35fd1a5159
Prahlad Yeri <prahladyeri@yahoo.com>
2018-05-16 02:00:32 +0530

    updates

--- config.py
+++
@@ -1,7 +0,0 @@
-# Enter duration in minutes
-schedule = [
-	{"name":"idle", "category":"", "duration":15},
-	{"name":"find jobs on upwork", "category": "freelance", "duration":25},
-]
-
-slot_interval = 25 #minutes
